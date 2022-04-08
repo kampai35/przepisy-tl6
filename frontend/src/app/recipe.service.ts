e1dcaaad80dcc1f7be012e9558e34d1471cc6a45
kampai35 <warchol355@gmail.com>
2022-04-08 09:07:28 +0200

    adjustment

--- frontend/src/app/recipe.service.ts
+++ frontend/src/app/recipe.service.ts
@@ -6,22 +6,13 @@ import { map } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class RecipeService {
-
-  baseURL = "http://localhost:3011";
-  //project: any;
+  baseURL = "http://024f-91-246-72-14.ngrok.io";
 
   constructor(private http: HttpClient) { }
 
   public recipeList():Observable<Irecipes[]>{
-    return this.http.get<{data: Irecipes[]}>(this.baseURL+'/recipes').pipe(
+    return this.http.get<{data: Irecipes[]}>(this.baseURL+'/recipes', {headers: { Authorization: '2b722726c5964d8465e1153c9ea0e0189c9fd2c76734e839bbd8c767efa3a0f1d8ce082ef0ed56' }}).pipe(
       map(response => response['data'])
       );
   }
-
-  // public recipeList(): Observable<Irecipes[]>{
-  //   return this.http.get<Irecipes[]>(this.baseURL+"");
-  // }
-  // public addRecipe(recipe: Irecipes): Observable<Irecipes>{
-  //   return this.http.post<Irecipes>(this.baseURL+"", recipe);
-  // }
 }
