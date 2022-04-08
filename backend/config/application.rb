906caa10cc1d9e2c1dbabc7f5e0bcd8f7de3f29c
kampai35 <warchol355@gmail.com>
2022-04-08 09:13:50 +0200

    adj

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -37,7 +37,7 @@ module App
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:4200'
+        origins '*'
         resource '*', headers: :any, methods: [:get, :post, :delete, :put, :patch, :options, :head]
       end
     end
