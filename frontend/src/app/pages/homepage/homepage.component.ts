6175cc8b76f019f2c3955a645279a1a41e704518
kampai35 <warchol355@gmail.com>
2022-04-25 21:45:05 +0200

    btn to list

--- frontend/src/app/pages/homepage/homepage.component.ts
+++ frontend/src/app/pages/homepage/homepage.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss']
 })
-export class HomepageComponent implements OnInit {
+export class HomepageComponent {
+  constructor(
+    private router: Router
+  ){}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  btnClickToList(){
+    this.router.navigateByUrl('/lista-przepisow');
   }
-
 }
